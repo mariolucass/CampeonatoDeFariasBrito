399e9824464da9a5b5bbd42329d9e7a2e254dbd7
Mario Lucas <mariolasdev@gmail.com>
2023-08-26 11:04:59 -0300

    fix: changed local Url for backend Url.

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -4,6 +4,6 @@ const baseURL = "https://back-end-projeto-farias-brito.vercel.app/api";
 const localUrl = "http://127.0.0.1:8000/api";
 
 export const api = axios.create({
-  baseURL: localUrl,
+  baseURL: baseURL,
   timeout: 12000,
 });
